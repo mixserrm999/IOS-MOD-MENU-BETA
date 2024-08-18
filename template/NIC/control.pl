ecbee1d438bbfcfe8da837c8ffb3049211ee45a8
TBMODS <61624005+mixserrm999@users.noreply.github.com>
2024-08-18 23:52:20 +0800

    Update control.pl

--- template/NIC/control.pl
+++ template/NIC/control.pl
@@ -4,4 +4,6 @@ NIC->prompt("APPNAME", "Enter name of the app", {default => ""});
 
 NIC->prompt("APPVERSION", "Enter current version of the app", {default => "1.0"});
 
+NIC->prompt("MAIN", "Enter the main .mm [Gestures/Button]", {default => "Button"});
+
 NIC->prompt("SITE", "Enter the site where the hack is for", {default => ""});
