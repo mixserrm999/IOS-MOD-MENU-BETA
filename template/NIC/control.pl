166fca0bd5c737d950144d54ef9f04e5b2164af1
TBMODS <61624005+mixserrm999@users.noreply.github.com>
2024-08-26 05:23:43 +0800

    Update control.pl

--- template/NIC/control.pl
+++ template/NIC/control.pl
@@ -1,14 +1,11 @@
-# Define project name and other details directly
-my $project_name = "SERMIX";
-my $binary_name = "";
-my $app_name = "SERMIX";
-my $app_version = "0.1.0";
-my $main_file = "Button";
-my $site = "";
+NIC->prompt("PROJECTNAME", "Project name", {default => "SERMIX"});
 
-# Set these values as default in NIC prompts
-NIC->prompt("BINARYNAME", "Enter binary name of the app (if you know it)", {default => $binary_name});
-NIC->prompt("APPNAME", "Enter name of the app", {default => $app_name});
-NIC->prompt("APPVERSION", "Enter current version of the app", {default => $app_version});
-NIC->prompt("MAIN", "Enter the main .mm [Gestures/Button]", {default => $main_file});
-NIC->prompt("SITE", "Enter the site where the hack is for", {default => $site});
+NIC->prompt("BINARYNAME", "Enter binary name of the app (if you know it)", {default => ""});
+
+NIC->prompt("APPNAME", "Enter name of the app", {default => "SERMIX"});
+
+NIC->prompt("APPVERSION", "Enter current version of the app", {default => "0.1.0"});
+
+NIC->prompt("MAIN", "Enter the main .mm [Gestures/Button]", {default => "Button"});
+
+NIC->prompt("SITE", "Enter the site where the hack is for", {default => ""});
