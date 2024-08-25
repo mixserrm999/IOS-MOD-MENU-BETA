ac92d4fef00cf14d7dd54ff79749a2f9dbbb9b60
TBMODS <61624005+mixserrm999@users.noreply.github.com>
2024-08-26 03:45:06 +0800

    oki

--- template/NIC/control.pl
+++ template/NIC/control.pl
@@ -1,8 +1,8 @@
 NIC->prompt("BINARYNAME", "Enter binary name of the app (if you know it)", {default => ""});
 
-NIC->prompt("APPNAME", "Enter name of the app", {default => ""});
+NIC->prompt("APPNAME", "Enter name of the app", {default => "SERMIX"});
 
-NIC->prompt("APPVERSION", "Enter current version of the app", {default => "1.0"});
+NIC->prompt("APPVERSION", "Enter current version of the app", {default => "0.1.0"});
 
 NIC->prompt("MAIN", "Enter the main .mm [Gestures/Button]", {default => "Button"});
 
